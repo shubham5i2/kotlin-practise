8aa5c12a4dc901d31181c317e044ef60c807cbb3
sonamsingh269806 <sonamsingh269806@gmail.com>
2023-10-17 20:00:03 +0530

    first commit

---
+++ src/main/kotlin/Variable.kt
@@ -0,0 +1,10 @@
+fun main() {
+//    val is for constants
+    val userName: String = "Shubham"
+    println("My name is $userName!")
+
+//    var can be changes
+    var city: String = "Visakhapatnam"
+    city = "Hyderabad"
+    println("I live in $city")
+}
