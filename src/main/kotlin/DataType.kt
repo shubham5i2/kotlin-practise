29b0e16641dfd1f6949fd76a4c16fa5b496fb7d6
sonamsingh269806 <sonamsingh269806@gmail.com>
2023-10-17 21:08:51 +0530

    data types

---
+++ src/main/kotlin/DataType.kt
@@ -0,0 +1,27 @@
+fun main() {
+    val byteMinValue = Byte.MIN_VALUE
+    val byteMaxValue = Byte.MAX_VALUE
+    println("Byte MIN value is $byteMinValue and MAX value is $byteMaxValue")
+
+    val shortMinValue: Short = Short.MIN_VALUE
+    val shortMaxValue: Short = Short.MAX_VALUE
+    println("Short MIN value is $shortMinValue and MAX value is $shortMaxValue")
+
+    val intMinValue: Int = Int.MIN_VALUE
+    val intMaxValue: Int = Int.MAX_VALUE
+    println("Integer MIN value is $intMinValue and MAX value is $intMaxValue")
+
+    val longMinValue: Long = Long.MIN_VALUE
+    val longMaxValue: Long = Long.MAX_VALUE
+    println("Long MIN value is $longMinValue and MAX value is $longMaxValue")
+
+    val floatNumber = 2.5f
+    val floatMinValue: Float = Float.MIN_VALUE
+    val floatMaxValue: Float = Float.MAX_VALUE
+    println("Float MIN value is $floatMinValue and MAX value is $floatMaxValue")
+
+    val doubleNumber = 2.5
+    val doubleMinValue: Double = Double.MIN_VALUE
+    val doubleMaxValue: Double = Double.MAX_VALUE
+    println("Double MIN value is $doubleMinValue and MAX value is $doubleMaxValue")
+}
--- src/main/kotlin/Variable.kt
+++ src/main/kotlin/Variable.kt
@@ -1,10 +1,14 @@
 fun main() {
 //    val is for constants
     val userName: String = "Shubham"
-    println("My name is $userName!")
 
 //    var can be changes
     var city: String = "Visakhapatnam"
     city = "Hyderabad"
-    println("I live in $city")
+
+    val age: Int = 26
+
+    // we don't need explicitly mention type 'Int'
+    val salary = 23000
+    println("My name is $userName. I am $age years old. My salary is $salary. I live in $city")
 }
