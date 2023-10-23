5db40d776541937f117cd59e9ae2b722611b8892
sonamsingh269806 <sonamsingh269806@gmail.com>
2023-10-23 15:02:28 +0530

    class and object

---
+++ src/main/kotlin/oops/classobjects/Car.kt
@@ -0,0 +1,44 @@
+package oops.classobjects
+
+fun main() {
+    val car = Car()
+    car.name = "Mercedes"
+    car.model = "S-Class"
+    car.color = "Black"
+
+    println("Name = ${car.name}")
+    println("Model = ${car.model}")
+    println("Color = ${car.color}")
+
+    car.move()
+    car.stop()
+
+    val bike = Bike("Hero Honda", "Splendor Plus", "70kmph")
+    bike.move()
+}
+
+//class with default constructor
+class Car {
+    var name = ""
+    var model = ""
+    var color = ""
+
+    fun move() {
+        println("The car $name is moving")
+    }
+
+    fun stop() {
+        println("The car $name has stopped")
+    }
+}
+
+//class with parametrized constructor
+// 'name' parameter is converted to a property
+class Bike(private var name: String, model: String, speed: String) {
+    var model = model
+    var speed = speed
+
+    fun move() {
+        println("$name - $model is moving with speed $speed")
+    }
+}
