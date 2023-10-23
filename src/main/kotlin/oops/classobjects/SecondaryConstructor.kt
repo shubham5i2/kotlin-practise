d7a84165ada4f5df8dda394879f8f94d65d63398
sonamsingh269806 <sonamsingh269806@gmail.com>
2023-10-23 18:42:59 +0530

    secondary constructor

---
+++ src/main/kotlin/oops/classobjects/SecondaryConstructor.kt
@@ -0,0 +1,27 @@
+package oops.classobjects
+
+fun main() {
+    val myself = User("John")
+    val friend = User("Doe", 23)
+    //Named Arguments
+    val course = Course(name = "Android Development", duration = 2)
+    val course1 = Course(name = "Full Stack Development")
+
+    println("My name is ${myself.name} and my best friend name is ${friend.name}")
+    println("${myself.name} is interested in ${course.name} which is of duration ${course.duration} months")
+    println("${friend.name} is interested in ${course1.name} which is of duration ${course1.duration} months")
+}
+
+class User(var name: String, var city: String, var age: Int) {
+
+    //secondary constructors
+    constructor(name: String) : this(name, "Visakhapatnam", 25)
+
+    constructor(name: String, age: Int) : this(name, "Visakhapatnam", age) {
+        println("This is secondary constructor")
+    }
+}
+
+class Course(var name: String, var duration: Int = 2) {
+    constructor(name: String) : this(name, 3)
+}
